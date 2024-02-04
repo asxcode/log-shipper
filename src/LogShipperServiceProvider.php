857a1faab3a9c4a9465e16070d7f1f2ee0915f74
Arvind Suthar <arvindsuthar198@gmail.com>
2024-02-04 16:42:55 +0530

    FIX: log-shipper channel added to Log::class

--- src/LogShipperServiceProvider.php
+++ src/LogShipperServiceProvider.php
@@ -8,17 +8,55 @@ class LogShipperServiceProvider extends ServiceProvider
 {
     protected $defer = true;
     
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
     public function boot()
     {
+        // Publish configuration file
         $this->publishes([
             __DIR__.'/../config/log-shipper.php' => config_path('log-shipper.php'),
         ], 'config');
+
+        // Merge configuration file
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/log-shipper.php', 'log-shipper'
+        );
+
+        // Automatically add the new channel to Laravel's logging configuration
+        $this->addLoggingChannel();
     }
 
     public function register()
     {
         $this->app->bind('log-shipper', function () {
-            return new LSLog();
+            return new LogShipperLogger();
         });
+    }
+
+    /**
+     * Add the new logging channel to Laravel's logging configuration.
+     *
+     * @return void
+     */
+    protected function addLoggingChannel()
+    {
+        // Get the existing channels from the Laravel configuration
+        $channels = config('logging.channels', []);
+
+        // Add the new channel if it doesn't exist
+        if (!isset($channels['log-shipper'])) {
+            $channels['log-shipper'] = [
+                'driver' => 'daily',
+                'path' => storage_path('logs/log-shipper.log'),
+                'level' => 'debug',
+                'days' => 14,
+            ];
+        }
+
+        // Update the modified configuration back to Laravel's config
+        config(['logging.channels' => $channels]);
     }
 }

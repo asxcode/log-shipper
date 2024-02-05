34da16e57ace533ab7dffb76b5eeffc6bb5cfd90
Arvind Suthar <arvindsuthar198@gmail.com>
2024-02-06 01:40:35 +0530

    ADD: ship command API post calls added

--- src/Commands/LogShipperShipCommand.php
+++ src/Commands/LogShipperShipCommand.php
@@ -3,6 +3,9 @@
 namespace Asxcode\LogShipper\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\File;
+
 
 class LogShipperShipCommand extends Command
 {
@@ -11,8 +14,46 @@ class LogShipperShipCommand extends Command
 
     public function handle()
     {
-        // log shipping logic goes here
         
-        $this->info('Logs have been shipped!');
+        $todayDate = now()->format('Y-m-d');
+
+        $directoryPath = storage_path('logs/log-shipper/' . $todayDate);
+
+        $files = File::files($directoryPath);
+
+        $logLevelMap = [
+            'error' => 'error',
+            'warning' => 'warning',
+            'debug' => 'debug',
+            'emergency' => 'emergency',
+            'info' => 'info'
+        ];
+        
+        foreach ($files as $file) {
+            $filename = pathinfo($file, PATHINFO_FILENAME);
+        
+            if (isset($logLevelMap[$filename])) {
+                $logLevel = $logLevelMap[$filename];
+                $this->shipToLogView($file, $logLevel);
+            }
+        }
+
+        $this->warn('Logs shipped!');
+    }
+
+    private function shipToLogView($logFilePath, $logLevel) {
+        $flaskEndpoint = env('LOGVIEW_ENDPOINT') . $logLevel;
+
+        $response = Http::attach(
+            'logs',
+            file_get_contents($logFilePath),
+            basename($logFilePath)
+        )->post($flaskEndpoint);
+        
+        if ($response->successful()) {
+            $this->info($logFilePath . " file sent successfully to logview.");
+        } else {
+            $this->error("Failed to send log file to logview. Status code: " . $response);
+        }
     }
 }
